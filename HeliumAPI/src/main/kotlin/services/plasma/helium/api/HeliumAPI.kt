40c44f1b33746d9c10873c11fa7d9cbdd3579ff3
JordanFails <simplyhatemc@gmail.com>
2024-08-21 18:28:54 -0400

    chore: fixed spacing

--- HeliumAPI/src/main/kotlin/services/plasma/helium/api/HeliumAPI.kt
+++ HeliumAPI/src/main/kotlin/services/plasma/helium/api/HeliumAPI.kt
@@ -4,74 +4,92 @@ import java.util.UUID
 
 enum class HeliumAPI {
     INSTANCE;
+
     @Throws(RuntimeException::class)
     fun isServerOnline(server: String): Boolean {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getServerState(server: String): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getOnlinePlayers(server: String): Int {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getMaxPlayers(server: String): Int {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getRankName(uuid: UUID): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getRankDisplayName(uuid: UUID): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getRankPrefix(uuid: UUID): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getRankSuffix(uuid: UUID): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getRankPriority(uuid: UUID): Int {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getRankColor(uuid: UUID): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getTagName(uuid: UUID): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getTagDisplayName(uuid: UUID): String {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun getCoins(uuid: UUID): Int {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun isAdminChat(uuid: UUID): Boolean {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun isStaffChat(uuid: UUID): Boolean {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun isMuted(uuid: UUID): Boolean {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun isBanned(uuid: UUID): Boolean {
         throw RuntimeException("HeliumAPI is not a plugin!")
     }
+
     @Throws(RuntimeException::class)
     fun isBlacklisted(uuid: UUID): Boolean {
         throw RuntimeException("HeliumAPI is not a plugin!")
